9d64213147ac1c58620fc3ad14bf8b8e853223c4
Hussein <hussienabed1111@gmail.com>
2025-10-05 21:42:56 +0300

    last updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,3 +202,25 @@
   });
 
 })();
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll("#navmenu ul li a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 100; // المسافة قبل بداية السكشن بشوية
+      const sectionHeight = section.clientHeight;
+
+      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === `#${current}`) {
+        link.classList.add("active");
+      }
+    });
+  });
